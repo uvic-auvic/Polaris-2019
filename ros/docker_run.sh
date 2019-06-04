f5da0c23a70c4fdaefd838efaa25fa55aed6ae73
Alec <avlecxk@gmail.com>
2019-06-04 13:11:58 -0700

    Updated docker scripts

--- ros/docker_run.sh
+++ ros/docker_run.sh
@@ -3,6 +3,8 @@ RED='\033[0;31m'
 GRN='\033[0;32m'
 NC='\033[0m'
 
+VIDEO_DEVICE=/dev/video0
+
 # Get latest image
 printf "${YEL}Ensuring image is up to date.${NC}\n"
 docker pull auvic/polaris:polaris_run
@@ -10,7 +12,7 @@ docker pull auvic/polaris:polaris_run
 # Check if the container has been created
 if [ ! "$( docker container ls -a | grep polaris_runtime)" ]; then
 	printf "${YEL}Docker container polaris_runtime ${RED}not found${YEL}, creating new container.${NC}\n"
-	docker run -it --name polaris_runtime --mount src="$(pwd)"/..,target=/usr/polaris,type=bind polaris_run
+	docker run -i -t --name polaris_runtime --mount src="$(pwd)"/..,target=/var/polaris,type=bind --device=${VIDEO_DEVICE}:/dev/video polaris_run
 else
 	printf "${YEL}Docker container polaris_runtime ${GRN}found${YEL}.${NC}\n"
 	docker start -a polaris_runtime
