5f184799405aa95bbf87d5ab6a2630781c1220b3
Robert Wasmann <rob@hypergalactic.ca>
2019-06-11 22:10:47 -0700

    (control_system) added some code snippets / ideas for pulling parameters from the Parameter server via a rosparam list. Not final in any way! Not looking worthwhile.

--- ros/src/controlsystem/src/control_system.cpp
+++ ros/src/controlsystem/src/control_system.cpp
@@ -1,4 +1,5 @@
 #include "ros/ros.h"
+#include <XmlRpcValue.h>
 #include <string>
 #include <map>
 
@@ -15,38 +16,73 @@ class StateMachine {
 
 class ControlSystem {
 private:
-  ros::NodeHandle& nodeHandle_;
+  ros::NodeHandle& nh_;
 
-  StateMachine stateMachine_;
+  StateMachine state_machine_;
 
 public:
-  ControlSystem(ros::NodeHandle& nodeHandle)
-    : nodeHandle_(nodeHandle)
+  controlSystem(ros::NodeHandle& nh)
+    : nh_(nh)
   {
-    // State processing.
-    nodeHandle_.getParam();
 
-  }
+    // parameter loading examples.
+    /*
+    XmlRpc::XmlRpcValue v;
+    nh_.param("subscribed_to_nodes", v, v);
+    for(int i =0; i < v.size(); i++)
+    {
+      node_names_.push_back(v[i]);
+      std::cerr << "node_names: " << node_names_[i] << std::endl;
+    }
+    */
 
-  int operator()() noexcept
-  {
-    while(ros::ok())
+    // code below is just a blob of crap and ideas that is looking too be far too complicated to do a simple thing.
+    XmlRpc::XmlRpcValue list1;
+    nh_.param("controlsystem", list1, list1);
+    ROS_ASSERT(list1.getType() == XmlRpc::XmlRpcValue::TypeStruct);
+
+    for(XmlRpc::XmlRpcValue::ValueStruct::const_iterator it = list1.begin(); it != list1.end(); ++it)
+    {
+      ROS_INFO_STREAM("Found params: " << (std::string)(it->first) << " ==> " << list1[it->first]);
+
+      XmlRpc::XmlRpcValue list2 = it->second;
+      ROS_ASSERT(list2.getType() == XmlRpc::XmlRpcValue::TypeStruct);
+      for(XmlRpc::XmlRpcValue::ValueStruct::const_iterator it2 = list2.begin();
+          it2 != list2.end(); ++it2)
       {
+        XmlRpc::XmlRpcValue xml_rpc_value = it2->first;
 
+        if (params[it->first] == "config") {
+          if (params[it2->first] == "pid_coeffs") {
+
+            if (xml_rpc_value.getType() != XmlRpc::XmlRpcValue::TypeDouble)
+              continue;
+
+            const double value = static_cast<double>(xml_rpc_value);
+            ROS_INFO_STREAM(it2->first << " " << value);
+          }
+        }
       }
-    return 0;
+    }
   }
+
+
 };
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv);
-  ros::NodeHandle nodeHandle("~");
+  ros::NodeHandle nh("~");
 
-  ControlSystem controlSystem(nh);
+  ControlSystem control_system();
 
-  int returnCode = controlSystem();
-
-  ros::shutdown();
-  return returnCode;
+  int operator()() noexcept
+  {
+    while(ros::ok())
+      {
+        int return_code = control_system(nh);
+      }
+    ros::shutdown();
+    return return_code;
+  }
 }
