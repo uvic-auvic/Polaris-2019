904a48ff7c248b24191cf1e131aede7a420ef6d5
anijjar <Amannijjar19@gmail.com>
2019-07-30 22:47:09 -0700

    Night 1_1
    amans first push to run examples

--- ros/src/controlsystem/src/statemachine.cpp
+++ ros/src/controlsystem/src/statemachine.cpp
@@ -12,6 +12,7 @@ void StateMachine::load_functors_(StateMachine::functormap_type& functor_map)
 	FUNC_INSERT("ProcedureB", procedures::ProcedureB());
 	FUNC_INSERT("IdleProcedure", procedures::IdleProcedure());
 	FUNC_INSERT("SurfaceProcedure", procedures::SurfaceProcedure());
+  FUNC_INSERT("RotateRightAngleProcedure", procedures::RotateRightAngleProcedure());
 }
 
   // Private member function used to check if a parameter.
