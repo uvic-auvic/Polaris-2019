755a147c43a0f17ec244054edc8ce94b3249baf9
robwasmann <robert.wasmann@gmail.com>
2020-03-15 02:35:16 -0700

    fix(github workflow) testing docker build script

--- ros/docker_make.sh
+++ ros/docker_make.sh
@@ -6,15 +6,15 @@ GRN='\033[0;32m'
 NC='\033[0m'
 
 # Get latest image
-printf "${YEL}Ensuring image is up to date.${NC}\n"
+#printf "${YEL}Ensuring image is up to date.${NC}\n"
 docker pull auvic/polaris:polaris_dev
 
 # Check if the container has been created
 if [ ! "$( docker container ls -a | grep polaris_development)" ]; then
-	printf "${YEL}Docker container ${RED}not found${YEL}, creating new container.${NC}\n"
+	#printf "${YEL}Docker container ${RED}not found${YEL}, creating new container.${NC}\n"
 	docker run -it --name polaris_development --mount src=$(pwd)/..,target=/var/polaris,type=bind auvic/polaris:polaris_dev
 else
-	printf "${YEL}Docker container ${GRN}found${YEL}.${NC}\n"
+	#printf "${YEL}Docker container ${GRN}found${YEL}.${NC}\n"
 	docker start -a polaris_development
 fi
-printf "${YEL}Done.${NC}\n"
+#printf "${YEL}Done.${NC}\n"
